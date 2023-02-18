81e106f8631fc18b3c99160f19dcea70b98d83c4
Mikhael Mounay Rais <81804418+MikhaelMounay@users.noreply.github.com>
2023-02-18 21:44:00 +0200

    Adjusted date-related function (datetime.timedelta)

--- webhook_api/flask_app.py
+++ webhook_api/flask_app.py
@@ -14,6 +14,8 @@ CLIENTS_PATH = "absolute_path_to_(clients.txt)_file"
 
 # Upcoming Birthdays Function
 birthdays = ""
+
+
 def upcoming_birthdays(request_type):
     global birthdays
     _RE_COMBINE_WHITESPACE = re.compile(r"\s+")
@@ -26,39 +28,34 @@ def upcoming_birthdays(request_type):
             return f"*{birthdays_dict[today]}*'s birthday is on _{today}_ 😊"
 
     elif request_type == "tomorrow":
-        upnext_day = f"{datetime.datetime.now().day + 1}/{datetime.datetime.now().month}"
-        if (upnext_day in birthdays_dict):
-            return f"*{birthdays_dict[upnext_day]}*'s birthday is on _{upnext_day}_ 😊"
+        tomorrow_date = datetime.datetime.now() + datetime.timedelta(days=1)
+        tomorrow = f"{tomorrow_date.day}/{tomorrow_date.month}"
+        if (tomorrow in birthdays_dict):
+            return f"*{birthdays_dict[tomorrow]}*'s birthday is on _{tomorrow}_ 😊"
 
     elif request_type == "upcoming":
-        day = datetime.datetime.now().day + 1
-        month = datetime.datetime.now().month
+        upcoming_date = datetime.datetime.now() + datetime.timedelta(days=1)
         while True:
-          tested_day = f"{day}/{month}"
-          if (tested_day in birthdays_dict):
-            return f"Closest Upcoming Birthday :\n\n*{birthdays_dict[tested_day]}*'s birthday is on _{tested_day}_ 🤩"
-          else:
-            if day < 32:
-              day += 1
+            tested_day = f"{upcoming_date.day}/{upcoming_date.month}"
+            if (tested_day in birthdays_dict):
+                return f"Closest Upcoming Birthday :\n\n*{birthdays_dict[tested_day]}*'s birthday is on _{tested_day}_ 🤩"
             else:
-              day = 1
-              if month < 12:
-                month += 1
-              else:
-                month = 1
+                upcoming_date += datetime.timedelta(days=1)
 
     elif request_type == "thismonth":
         thismonth_birthdays = "This Month's Upcoming Birthdays (Total _totalhere_):"
         total = 0
         for i in range(1, 32):
-          tested_day = f"{i}/{datetime.datetime.now().month}"
-          if (tested_day in birthdays_dict):
-            thismonth_birthdays += f"\n\n*{birthdays_dict[tested_day]}*'s birthday is on _{tested_day}_ 🤩"
-            total += 1
+            tested_day = f"{i}/{datetime.datetime.now().month}"
+            if (tested_day in birthdays_dict):
+                thismonth_birthdays += f"\n\n*{birthdays_dict[tested_day]}*'s birthday is on _{tested_day}_ 🤩"
+                total += 1
         thismonth_birthdays = thismonth_birthdays.replace("totalhere", str(total))
         return thismonth_birthdays
 
 # Handlers
+
+
 def upcoming(update, context):
     chat_id = update.message.chat.id
     pinned_msg = bot.get_chat(chat_id=chat_id).pinned_message
@@ -66,12 +63,14 @@ def upcoming(update, context):
     birthdays = str(pinned_msg.text.replace("\n", ""))
     update.message.reply_text(upcoming_birthdays("upcoming"), parse_mode=telegram.ParseMode.MARKDOWN)
 
+
 def thismonth(update, context):
     chat_id = update.message.chat.id
     pinned_msg = bot.get_chat(chat_id=chat_id).pinned_message
     global birthdays
     birthdays = str(pinned_msg.text.replace("\n", ""))
     update.message.reply_text(upcoming_birthdays("thismonth"), parse_mode=telegram.ParseMode.MARKDOWN)
+
 
 def today(update, context):
     chat_id = update.message.chat.id
@@ -84,6 +83,7 @@ def today(update, context):
     else:
         update.message.reply_text("Just take care of yourself for today 🥰", parse_mode=telegram.ParseMode.MARKDOWN)
 
+
 def tomorrow(update, context):
     chat_id = update.message.chat.id
     pinned_msg = bot.get_chat(chat_id=chat_id).pinned_message
@@ -95,8 +95,9 @@ def tomorrow(update, context):
     else:
         update.message.reply_text("You don't trust me then 🙄.\n_No birthdays for tomorrow._", parse_mode=telegram.ParseMode.MARKDOWN)
 
+
 def birthday(update, context):
-    start_message = update.message.reply_text("{\n\"1/" + str(datetime.datetime.now().month) +"\": \"Name1\",\n\"20/" + str(datetime.datetime.now().month) + "\": \"Name2\"\n}", parse_mode=telegram.ParseMode.MARKDOWN)
+    start_message = update.message.reply_text("{\n\"1/" + str(datetime.datetime.now().month) + "\": \"Name1\",\n\"20/" + str(datetime.datetime.now().month) + "\": \"Name2\"\n}", parse_mode=telegram.ParseMode.MARKDOWN)
     start_message.chat.unpin_all_messages()
     update.message.reply_text("_Fill your own list on the same pattern and *PIN* it.\nPlease try not to add too much extra *spaces* or *indentations* to avoid breaking the list!!_", parse_mode=telegram.ParseMode.MARKDOWN, reply_to_message_id=start_message.message_id)
     update.message.reply_text("*Send me your list of memorable birthdays, and I promise I won't forget a single one* 😊", parse_mode=telegram.ParseMode.MARKDOWN)
@@ -107,15 +108,18 @@ def birthday(update, context):
         if (str(update.message.chat.id) not in clients_id):
             add_new_client_id(update.message.chat.id)
 
+
 def add_new_client_id(new_id):
     with open(CLIENTS_PATH, "a") as clients:
         clients.write(str(new_id) + "\n")
+
 
 def set_birthdays(update, context):
     msg = update.message
     msg.chat.unpin_all_messages()
     new_msg = update.message.reply_text(msg.text)
     new_msg.pin()
+
 
 # Flask API & Webhook
 # Dispatcher
@@ -136,12 +140,15 @@ bot.set_webhook(url=url)
 
 # Process Updates
 app = Flask(__name__)
+
+
 @app.route("/" + SECRET, methods=["POST"])
 def telegram_webhook():
     json_string = request.stream.read().decode("utf-8")
     update = telegram.Update.de_json(json.loads(json_string), bot)
     dp.process_update(update)
     return "OK", 200
+
 
 @app.route("/" + SECRET + "/daily-reminder", methods=["POST"])
 def daily_reminder():
